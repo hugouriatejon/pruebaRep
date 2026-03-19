7c43c7d7b202dd2a9f714d6febff296bbc883716
hugouriatejon <0611687284@uma.es>
2026-03-19 03:22:58 +0100

    owner modifies constructor

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,7 +19,6 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-  private int variableColaborador;
 
   /**
    * Retrieves the day component of this date.
@@ -56,10 +55,11 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  //Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+	  this.day = dia;
+	  this.month = mes;
+	  this.year = año;
   }
 
   /**
